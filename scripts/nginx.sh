b49df00a3b897862dcb48906f77a055d8f21e82e
Vinay23107 <157478658+Vinay23107@users.noreply.github.com>
2024-04-21 02:54:39 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/restaurant/nginx/nginx.conf /etc/nginx/sites-available/restaurant
-sudo ln -s /etc/nginx/sites-available/restaurant /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/landlottery/nginx/nginx.conf /etc/nginx/sites-available/landlottery
+sudo ln -s /etc/nginx/sites-available/landlottery /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
